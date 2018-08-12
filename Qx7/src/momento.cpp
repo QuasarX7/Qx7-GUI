43464d09cbed36bd1e769a3798c33fe0e947e6b5
QuasarX7 <quasar_x7@yahoo.it>
2018-08-12 09:14:41 +0200

    generalizzazione delle classi temporali

--- Qx7/src/momento.cpp
+++ Qx7/src/momento.cpp
@@ -1,3 +1,52 @@
+/****************************************************************************
+ * Copyright (C) 2017-2018 by Dott. Domenico della Peruta                   *
+ *                                                                          *
+ * (ITALIANO)                                                               *
+ * Questo file è parte di Qx7.                                              *
+ *                                                                          *
+ * Qx7 è una libreria software gratuita: puoi ridistribuirlo e/o modificarlo*
+ * sotto i termini della GNU Lesser General Public License come pubblicato  *
+ * dalla Free Software Foundation, o la versione 3 della licenza, o         *
+ * (a tua scelta) qualsiasi versione successiva.                            *
+ *                                                                          *
+ * Qx7 è distribuita nella speranza che sia utile,                          *
+ * ma SENZA ALCUNA GARANZIA; senza nemmeno la garanzia implicita di         *
+ * COMMERCIABILITÀ O IDONEITÀ PER UN PARTICOLARE SCOPO. Vedi il             *
+ * GNU Lesser General Public License per maggiori dettagli.                 *
+ *                                                                          *
+ * Dovresti aver ricevuto una copia di GNU Lesser General Public            *
+ * Licenza insieme a Qx7. Altrimenti, vedi al sito                          *
+ * <http://www.gnu.org/licenses/>.                                          *
+ *                                                                          *
+ * (ENGLISH)                                                                *
+ * This file is part of Qx7.                                                *
+ *                                                                          *
+ *   Qx7 is free software: you can redistribute it and/or modify it         *
+ *   under the terms of the GNU Lesser General Public License as published  *
+ *   by the Free Software Foundation, either version 3 of the License, or   *
+ *   (at your option) any later version.                                    *
+ *                                                                          *
+ *   Qx7 is distributed in the hope that it will be useful,                 *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *   GNU Lesser General Public License for more details.                    *
+ *                                                                          *
+ *   You should have received a copy of the GNU Lesser General Public       *
+ *   License along with Qx7.  If not, see                                   *
+ *   <http://www.gnu.org/licenses/>.                                        *
+ ****************************************************************************/
+
+/**
+ * @file momento.cpp
+ * @author Dr. Domenico della Peruta
+ * @date 11-08-2018
+ * @version 1.0.0, 11-08-2018
+ *
+ * @brief File contenente l'implementazione dei metodi della classe astratta Momento.
+ *
+ * @namespace Utili
+ * @class Momento
+ */
 #include "momento.h"
 
 using namespace Utili;
--- Qx7/src/momento.h
+++ Qx7/src/momento.h
@@ -1,3 +1,51 @@
+/****************************************************************************
+ * Copyright (C) 2017-2018 by Dott. Domenico della Peruta                   *
+ *                                                                          *
+ * (ITALIANO)                                                               *
+ * Questo file è parte di Qx7.                                              *
+ *                                                                          *
+ * Qx7 è una libreria software gratuita: puoi ridistribuirlo e/o modificarlo*
+ * sotto i termini della GNU Lesser General Public License come pubblicato  *
+ * dalla Free Software Foundation, o la versione 3 della licenza, o         *
+ * (a tua scelta) qualsiasi versione successiva.                            *
+ *                                                                          *
+ * Qx7 è distribuita nella speranza che sia utile,                          *
+ * ma SENZA ALCUNA GARANZIA; senza nemmeno la garanzia implicita di         *
+ * COMMERCIABILITÀ O IDONEITÀ PER UN PARTICOLARE SCOPO. Vedi il             *
+ * GNU Lesser General Public License per maggiori dettagli.                 *
+ *                                                                          *
+ * Dovresti aver ricevuto una copia di GNU Lesser General Public            *
+ * Licenza insieme a Qx7. Altrimenti, vedi al sito                          *
+ * <http://www.gnu.org/licenses/>.                                          *
+ *                                                                          *
+ * (ENGLISH)                                                                *
+ * This file is part of Qx7.                                                *
+ *                                                                          *
+ *   Qx7 is free software: you can redistribute it and/or modify it         *
+ *   under the terms of the GNU Lesser General Public License as published  *
+ *   by the Free Software Foundation, either version 3 of the License, or   *
+ *   (at your option) any later version.                                    *
+ *                                                                          *
+ *   Qx7 is distributed in the hope that it will be useful,                 *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *   GNU Lesser General Public License for more details.                    *
+ *                                                                          *
+ *   You should have received a copy of the GNU Lesser General Public       *
+ *   License along with Qx7.  If not, see                                   *
+ *   <http://www.gnu.org/licenses/>.                                        *
+ ****************************************************************************/
+/**
+ * @file momento.h
+ * @author Dr. Domenico della Peruta
+ * @date 11-08-2018
+ * @version 1.0.0, 11-08-2018
+ *
+ * @brief File contenente l'intestazione della classe astratta Momento.
+ *
+ * @namespace Utili
+ * @class Momento
+ */
 #ifndef SRC_MOMENTO_H_
 #define SRC_MOMENTO_H_
 
@@ -19,7 +67,7 @@ class Momento {
 public:
 	virtual ~Momento(){}
 
-	virtual float valore(UnitàTemporale tipo)override const =0;
+	virtual float valore(UnitàTemporale tipo)const =0;
 
 	float differenza(const Momento& confronto, UnitàTemporale tipo)const{
 		return valore(tipo) - confronto.valore(tipo);
