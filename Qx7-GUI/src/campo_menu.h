09863e2b0afc59bdf9b88543f387ae812101413f
QuasarX7 <quasar_x7@yahoo.it>
2018-08-16 14:41:44 +0200

    descrizione file campo_menu

--- Qx7-GUI/src/campo_menu.h
+++ Qx7-GUI/src/campo_menu.h
@@ -1,3 +1,51 @@
+/****************************************************************************
+ * Copyright (C) 2017-2018 by Dott. Domenico della Peruta                   *
+ *                                                                          *
+ * (ITALIANO)                                                               *
+ * Questo file è parte di Qx7-GUI.                                          *
+ *                                                                          *
+ * Qx7-GUI è una libreria software gratuita: puoi ridistribuirla e/o        *
+ * modificarla sotto i termini della GNU Lesser General Public License come *
+ * pubblicato  dalla Free Software Foundation, o la versione 3 della licenza,
+ *  o (a tua scelta) qualsiasi versione successiva.                         *
+ *                                                                          *
+ * Qx7-GUI è distribuita nella speranza che sia utile,                      *
+ * ma SENZA ALCUNA GARANZIA; senza nemmeno la garanzia implicita di         *
+ * COMMERCIABILITÀ O IDONEITÀ PER UN PARTICOLARE SCOPO. Vedi il             *
+ * GNU Lesser General Public License per maggiori dettagli.                 *
+ *                                                                          *
+ * Dovresti aver ricevuto una copia di GNU Lesser General Public            *
+ * Licenza insieme a Qx7-GUI. Altrimenti, vedi al sito                      *
+ * <http://www.gnu.org/licenses/>.                                          *
+ *                                                                          *
+ * (ENGLISH)                                                                *
+ * This file is part of Qx7-GUI.                                            *
+ *                                                                          *
+ *   Qx7-GUI is free software: you can redistribute it and/or modify it     *
+ *   under the terms of the GNU Lesser General Public License as published  *
+ *   by the Free Software Foundation, either version 3 of the License, or   *
+ *   (at your option) any later version.                                    *
+ *                                                                          *
+ *   Qx7-GUI is distributed in the hope that it will be useful,             *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *   GNU Lesser General Public License for more details.                    *
+ *                                                                          *
+ *   You should have received a copy of the GNU Lesser General Public       *
+ *   License along with Qx7-GUI.  If not, see                               *
+ *   <http://www.gnu.org/licenses/>.                                        *
+ ****************************************************************************/
+/**
+ * @file  campo_menu.h
+ * @author Dr. Domenico della Peruta
+ * @date 16-08-2018
+ * @version 1.0.0, 16-08-2018
+ *
+ * @brief File contenente l'intestazione e l'implementazione della classe CampoMenu.
+ *
+ * @namespace GUI
+ * @class CampoMenu
+ */
 #ifndef SRC_CAMPO_MENU_H_
 #define SRC_CAMPO_MENU_H_
 
@@ -13,7 +61,10 @@ namespace GUI {
 
 class CampoMenu;
 typedef shared_ptr<CampoMenu> pCampoMenu;
-
+/**
+ * La classe CampoMenu è un campo di input con l'aggiunta di un menu a tendina con i possibili
+ * valori da inserire.
+ */
 class CampoMenu: public Campo {
 public:
 	/**
